ff48c69562e6308ba1c8cf519e117dc0cb6449a5
Paul Colomiets <paul@colomiets.name>
2016-12-01 13:05:28 +0200

    message-board: Add CLI parameter to change backend address

--- examples/message-board/messageboard/main.py
+++ examples/message-board/messageboard/main.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import uvloop
+import argparse
 from censusname import generate as make_name
 from sanic import Sanic
 from sanic.response import json as response
@@ -9,10 +10,16 @@ from .convention import swindon_convention
 from .swindon import connect
 
 def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--port', default=8082, help="Listen port")
+    ap.add_argument('--swindon-port', default=8081,
+        help="Connect to swindon at port")
+    options = ap.parse_args()
+
     logging.basicConfig(level=logging.DEBUG)
     loop = uvloop.new_event_loop()
     app = Sanic('messageboard')
-    swindon = connect(('localhost', 8081), loop=loop)
+    swindon = connect(('localhost', options.swindon_port), loop=loop)
 
     @app.route("/tangle/authorize_connection", methods=['POST'])
     @swindon_convention
@@ -36,8 +43,9 @@ def main():
 
 
     if os.environ.get("LISTEN_FDS") == '1':
+        # Systemd socket activation protocol
         import socket
         sock = socket.fromfd(3, socket.AF_INET, socket.SOCK_STREAM)
         app.run(host=None, port=None, sock=sock, loop=loop)
     else:
-        app.run(host="0.0.0.0", port=8082, loop=loop)
+        app.run(host="0.0.0.0", port=options.port, loop=loop)
