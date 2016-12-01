282b055797740a53a9d32abbfe246e3611db664d
Paul Colomiets <paul@colomiets.name>
2016-12-01 13:17:10 +0200

    multi-user-chat: Adds settings similar to ones added to message-board

--- examples/multi-user-chat/muc/main.py
+++ examples/multi-user-chat/muc/main.py
@@ -14,10 +14,16 @@ NON_ALPHA = re.compile('[^a-z0-9_]')
 
 
 def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--port', default=8082, help="Listen port")
+    ap.add_argument('--swindon-port', default=8081,
+        help="Connect to swindon at port")
+    options = ap.parse_args()
+
     logging.basicConfig(level=logging.DEBUG)
     loop = uvloop.new_event_loop()
     app = Sanic('multi-user-chat')
-    swindon = connect(('localhost', 8081), loop=loop)
+    swindon = connect(('localhost', options.swindon_port), loop=loop)
 
     @app.route("/tangle/authorize_connection", methods=['POST'])
     @swindon_convention
@@ -76,4 +82,10 @@ def main():
     async def get_history(req, room, start=0):
         return chat.get_room(room).get_history(start)
 
-    app.run(host="0.0.0.0", port=8082, loop=loop)
+    if os.environ.get("LISTEN_FDS") == '1':
+        # Systemd socket activation protocol
+        import socket
+        sock = socket.fromfd(3, socket.AF_INET, socket.SOCK_STREAM)
+        app.run(host=None, port=None, sock=sock, loop=loop)
+    else:
+        app.run(host="0.0.0.0", port=options.port, loop=loop)
