0bd6b1022efb196aeb81e6f0eea57a54d0af1bbe
Alexey Popravka <a.popravka@smartweb.com.ua>
2017-02-16 17:09:04 +0200

    more tests

--- tests/swindon_chat_test.py
+++ tests/swindon_chat_test.py
@@ -1,5 +1,7 @@
+import pytest
 from async_timeout import timeout
 from aiohttp import web
+from aiohttp import WSMsgType
 
 
 async def test_simple_userinfo(proxy_server, swindon):
@@ -37,3 +39,39 @@ async def test_ws_close_timeout(proxy_server, swindon):
             msg = await ws.receive_json()
             assert msg == [
                 'hello', {}, {'user_id': 'user:1'}]
+
+
+@pytest.mark.parametrize('status_code', [
+    400, 401, 404, 410, 500, 501, 502, 504])
+async def test_error_codes(proxy_server, swindon, loop, status_code):
+    url = swindon.url / 'swindon-chat'
+    async with proxy_server.swindon_chat(url, timeout=1) as inflight:
+        req, fut = await inflight.req.get()
+        assert req.path == '/tangle/authorize_connection'
+        fut.set_result(
+            web.Response(status=status_code, body=b'Custom Error'))
+        ws = await inflight.client_resp
+        msg = await ws.receive()
+        assert msg.type == WSMsgType.CLOSE
+        assert msg.data == 4000 + status_code
+        assert msg.extra == 'backend_error'
+        assert ws.closed
+        assert ws.close_code == 4000 + status_code
+
+
+@pytest.mark.parametrize('status_code', [
+    100, 101, 201, 204, 300, 301, 302, 304])
+async def test_unexpected_responses(proxy_server, swindon, loop, status_code):
+    url = swindon.url / 'swindon-chat'
+    async with proxy_server.swindon_chat(url, timeout=1) as inflight:
+        req, fut = await inflight.req.get()
+        assert req.path == '/tangle/authorize_connection'
+        fut.set_result(
+            web.Response(status=status_code, body=b'no body'))
+        ws = await inflight.client_resp
+        msg = await ws.receive()
+        assert msg.type == WSMsgType.CLOSE
+        assert msg.data == 4500
+        assert msg.extra == 'backend_error'
+        assert ws.closed
+        assert ws.close_code == 4500
