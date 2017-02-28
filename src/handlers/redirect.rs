190120706a6c07692238df6a7854730cd667184d
Alexey Popravka <a.popravka@smartweb.com.ua>
2017-02-28 18:38:53 +0200

    check host prefix on www redirect

--- src/handlers/redirect.rs
+++ src/handlers/redirect.rs
@@ -1,4 +1,5 @@
 use std::sync::Arc;
+use std::ascii::AsciiExt;
 
 use minihttp::Status;
 use tokio_core::io::Io;
@@ -12,29 +13,35 @@ use incoming::{reply, Request, Input};
 pub fn base_redirect<S: Io + 'static>(settings: &Arc<BaseRedirect>, inp: Input)
     -> Request<S>
 {
-    serve_redirect(settings.redirect_to_domain.as_str(), inp)
+    serve_redirect(settings.redirect_to_domain.as_str(), Status::Found, inp)
 }
 
 
 pub fn strip_www_redirect<S: Io + 'static>(inp: Input)
     -> Request<S>
 {
-    let dest = inp.headers.host()
-        .and_then(|host| host.splitn(2, '.').last());
-    match dest {
-        Some(host) => serve_redirect(host, inp),
-        None => serve_error_page(Status::InternalServerError, inp),
+
+    let base_host = inp.headers.host().and_then(|h| {
+        if h.len() > 4 && h[0..4].eq_ignore_ascii_case("www.") {
+            Some(h.split_at(4).1)
+        } else {
+            None
+        }
+    });
+    match base_host {
+        Some(host) => serve_redirect(host, Status::MovedPermanently, inp),
+        None => serve_error_page(Status::NotFound, inp),
     }
 }
 
 
-fn serve_redirect<S: Io + 'static>(host: &str, inp: Input)
+fn serve_redirect<S: Io + 'static>(host: &str, status: Status, inp: Input)
     -> Request<S>
 {
     // TODO: properly identify request scheme
     let dest = format!("http://{}{}", host, inp.headers.path().unwrap_or("/"));
     reply(inp, move |mut e| {
-        e.status(Status::Found);
+        e.status(status);
         e.add_header("Location", dest);
         e.add_length(0);
         if e.done_headers() {
