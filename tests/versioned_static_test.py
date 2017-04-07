6b19c10afeec3d6d454b0716e7575bcd33b75f6e
Paul Colomiets <paul@colomiets.name>
2017-04-07 13:31:11 +0300

    Add file path checks into versioned tests

--- tests/versioned_static_test.py
+++ tests/versioned_static_test.py
@@ -18,6 +18,11 @@ async def test_by_version1(swindon, http_request, debug_routing, path):
         'test.html?r=aabbbbbb')
     assert resp.status == 200
     assert resp.headers['Content-Type'] == 'text/html'
+    if debug_routing:
+        assert resp.headers['X-Swindon-File-Path'] == \
+            '"{}/hashed/aa/bbbbbb-test.html.html"'.format(TESTS_DIR)
+    else:
+        assert 'X-Swindon-File-Path' not in resp.headers
     assert data == b'<!DOCTYPE html>\n<title>Hello</title>\n'
 
 
@@ -27,37 +32,62 @@ async def test_by_version2(swindon, http_request, debug_routing, path):
         'test.html?r=bbaaaaaa')
     assert resp.status == 200
     assert resp.headers['Content-Type'] == 'text/html'
+    if debug_routing:
+        assert resp.headers['X-Swindon-File-Path'] == \
+            '"{}/hashed/bb/aaaaaa-test.html"'.format(TESTS_DIR)
+    else:
+        assert 'X-Swindon-File-Path' not in resp.headers
     assert data == b'<!DOCTYPE html>\n<title>Greetings</title>\n'
 
 
 @pytest.mark.parametrize("path", ALL_EQUAL)
 async def path_encoding(swindon, http_request, debug_routing, path):
     resp, data = await http_request(swindon.url / path /
-        'a+b.html?r=aabbbbbb')
+        'a+b.txt?r=aabbbbbb')
     assert resp.status == 200
     assert resp.headers['Content-Type'] == 'text/plain'
+    if debug_routing:
+        assert resp.headers['X-Swindon-File-Path'] == \
+            '"{}/hashed/aa/bbbbbb-a+b.txt"'.format(TESTS_DIR)
+    else:
+        assert 'X-Swindon-File-Path' not in resp.headers
     assert data == b'a+b at aabbbbbb\n'
 
 
 async def path_encoding_fallback(swindon, http_request, debug_routing, path):
     resp, data = await http_request(swindon.url / path /
-        'a+b.html')
+        'a+b.txt')
     assert resp.status == 200
     assert resp.headers['Content-Type'] == 'text/plain'
+    if debug_routing:
+        assert resp.headers['X-Swindon-File-Path'] == \
+            '"{}/assets/a+b.txt"'.format(TESTS_DIR)
+    else:
+        assert 'X-Swindon-File-Path' not in resp.headers
     assert data == b'a+b\n'
 
 async def path_other_params(swindon, http_request, debug_routing, path):
     resp, data = await http_request(swindon.url / path /
-        'a+b.html?some=param&another=param')
+        'a+b.txt?some=param&another=param')
     assert resp.status == 200
     assert resp.headers['Content-Type'] == 'text/plain'
+    if debug_routing:
+        assert resp.headers['X-Swindon-File-Path'] == \
+            '"{}/assets/a+b.txt"'.format(TESTS_DIR)
+    else:
+        assert 'X-Swindon-File-Path' not in resp.headers
     assert data == b'a+b\n'
 
 async def path_crappy_query(swindon, http_request, debug_routing, path):
     resp, data = await http_request(swindon.url / path /
-        'a+b.html?just_some_garbage')
+        'a+b.txt?just_some_garbage')
     assert resp.status == 200
     assert resp.headers['Content-Type'] == 'text/plain'
+    if debug_routing:
+        assert resp.headers['X-Swindon-File-Path'] == \
+            '"{}/assets/a+b.txt"'.format(TESTS_DIR)
+    else:
+        assert 'X-Swindon-File-Path' not in resp.headers
     assert data == b'a+b\n'
 
 
