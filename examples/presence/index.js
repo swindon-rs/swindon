4ae3636293c32c140e6bb9fdc0cd3b92659275a9
Paul Colomiets <paul@colomiets.name>
2017-08-23 13:30:13 +0300

    Adds stub for "presence" example

---
+++ examples/presence/index.js
@@ -0,0 +1,4 @@
+var buttonComponent = require('./button');
+
+buttonComponent.renderSync({})
+    .appendTo(document.body);
---
+++ examples/presence/webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public/js')
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/,
+        }, {
+            loader: 'marko-loader',
+            test: /\.marko$/,
+        }],
+    },
+    resolve: {
+        modules: process.env.NODE_PATH.split(':').concat('node_modules'),
+        extensions: ['.js', '.marko'],
+        mainFields: ['browser', 'jsnext:main', 'main'],
+    },
+    resolveLoader: {
+        modules: process.env.NODE_PATH.split(':').concat('node_modules'),
+        mainFields: ['jsnext:main', 'main'],
+    },
+};
