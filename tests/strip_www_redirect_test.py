530d153c66546bcb923caa2813b4f4dc9d7d612c
Paul Colomiets <paul@colomiets.name>
2017-08-26 00:40:58 +0300

    Fixes broken tests

--- tests/strip_www_redirect_test.py
+++ tests/strip_www_redirect_test.py
@@ -1,12 +1,13 @@
 import aiohttp
 
 
-async def test_ok(swindon, request_method, http_version, debug_routing, loop):
+async def test_ok(swindon, proxy_request_method, http_version,
+        debug_routing, loop):
     url = 'http://www.example.com:{}/empty.gif'.format(swindon.url.port)
     kw = {"allow_redirects": False}
 
     async with aiohttp.ClientSession(version=http_version, loop=loop) as s:
-        async with s.request(request_method, url, **kw) as resp:
+        async with s.request(proxy_request_method, url, **kw) as resp:
             assert resp.status == 301
             assert resp.headers.getall("Location") == [
                 "http://example.com:{}/empty.gif".format(swindon.url.port)
